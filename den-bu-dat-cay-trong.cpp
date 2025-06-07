dc2016df6b96220f1131f52f697981c65fa53a64
taitapcode <hoangductai2007@gmail.com>
2025-06-07 21:35:43 +0700

    Add solutions for UIT Contest 2025 p1

---
+++ den-bu-dat-cay-trong.cpp
@@ -0,0 +1,33 @@
+// Problem: https://thilaptrinh.uit.edu.vn/wecode/assignment/4/8
+#include<bits/stdc++.h>
+#define el '\n'
+#define ll long long
+#define db double
+#define ldb long double
+using namespace std;
+
+int n;
+ll mul = 1;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+
+  cin >> n;
+
+  int a;
+  while (n--)
+  {
+    cin >> a;
+    mul *= a;
+  }
+
+  ll sqrtMul = sqrt(mul);
+  if (sqrtMul * sqrtMul == mul)
+    cout << "Yes" << el;
+  else
+    cout << "No" << el;
+
+  return 0;
+}
